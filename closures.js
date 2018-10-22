3a51358ac220a5265a3751e5ef657d979f715ef9
Leo Tejeda <leonelhtejeda@gmail.com>
2018-10-22 10:04:08 -0700

    Clousers within

--- closures.js
+++ closures.js
@@ -24,3 +24,24 @@ function outer(){
 
    let storeOuter = outer(5)
    storeOuter(10); //
+
+   function classRoom(){
+       let instructors = ["Colt", "Elie"]
+       return {
+           getInstructors: function(){
+               return instructors;
+           },
+           addInstructor: function(instructor){
+               instructors.push(instructor)
+               return instructors
+           }
+       }
+   }
+
+coursel = classRoom();
+coursel.getInstructors()// ["Elie", "Colt"]
+coursel.addInstructor("Ian")// ["Elie", "Colt", "Ian"]
+coursel.getInstructors()// ["Elie", "Colt", "Ian"]
+
+course2 = classRoom()
+course2.getInstructors() // ["Elie", "Colt"] = not affected by coursel
